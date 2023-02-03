b931f1d27a3500acd20eea7052097859739fab0e
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2023-02-02 23:19:21 -0500

    Changing responsive degisn layout and position of add button

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const NoteItem = ({note}) => {
   return (
-    <Link to={`/edit-note/${note.id}`} className='bg-teal-500 flex flex-col p-4 gap-4 odd:row-span-4 odd:bg-fuchsia-500 even:col-span-1'>
+    <Link to={`/edit-note/${note.id}`} className='bg-teal-500 flex flex-col p-4 gap-4  odd:bg-fuchsia-500 '>
         <h4 className='text-white'>{note.title.length > 50 ? note.title.substr(0,50)+'...' : note.title}</h4>
         <p className='text-white text-sm '>{note.date}</p>
     </Link>
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -30,18 +30,18 @@ const Notes = ({notes}) => {
 
   return (
     <section >
-    <header className='flex justify-between items-center pl-5 pr-5 pb-4'>
-    {!search && <h2 className='text-white'>My Notes</h2>}
+    <header className='flex justify-between items-center pl-5 pr-5 pb-4 sm:fixed sm:top-0 sm:left=0 sm:w-full sm:py-8 sm:px-6'>
+    {!search && <h2 className='text-amber-500'>My Notes</h2>}
     {search && <input className ='border-opacity-100 bg-transparent border-2 border-gray-600 w-full pb-2 pt-2 pl-2 mr-4 text-xs text-white'type="text" autoFocus placeholder="Keyword..." value={searchValue} onChange={(e)=>setSearchValue(e.target.value)} />}
      <button className='text-white shadow-sm p-3 bg-gray-600 rounded-xl' onClick={()=>setSearch(prevSearchState=>!prevSearchState)} >{search?<AiOutlineCloseCircle/>:<BsSearch/>}</button>
     </header>
-    <div className="grid grid-cols-2 gap-4 ">
-        {filteredNotes.length === 0 &&<p className='text-white absolute left-20 top-1/2'>No notes found. </p>}
+    <div className="grid grid-cols-2 gap-4 sm:mt-24 ">
+        {filteredNotes.length === 0 &&<p className='text-white absolute left-[8.5rem] top-1/2'>No notes found. </p>}
         {
           filteredNotes.map(note => <NoteItem key={note.id} note={note}/>)
         }
     </div>
-   <Link to='/create-note' className='text-white shadow-sm p-3 bg-gray-500 rounded-xl bottom-16 right-28 fixed sm:top-8 h-10' >
+   <Link to='/create-note' className='text-white shadow-sm p-3 bg-gray-500 rounded-xl bottom-16 right-28 fixed sm:bottom-[6%] sm:right-8 h-10' >
    <GrAddCircle  />
    </Link>
   </section>
